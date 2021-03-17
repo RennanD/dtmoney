4a6bce23f9e7b9b80c46f727c79813ab1fea1b77
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-17 17:22:27 -0300

    feat: Create Modal styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+
+import closeImg from '../../assets/close.svg';
 
 type NewTransactionModalProps = {
   isNewTransacationModalOpen: boolean;
@@ -12,9 +15,25 @@ export function NewTransactionsModal({
   return (
     <Modal
       isOpen={isNewTransacationModalOpen}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
       onRequestClose={onToggleNewTransactionModal}
     >
-      <h2>Cadastrar transação </h2>
+      <button type="button" className="react-modal-close">
+        <img src={closeImg} alt="Close" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação </h2>
+
+        <input type="text" placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">Enviar</button>
+      </Container>
     </Modal>
   );
 }
