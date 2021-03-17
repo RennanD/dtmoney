7b02a2ae7e877b12e14a4ed695b2d0074e134fde
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-17 12:53:11 -0300

    feat: Configure MirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => [
+      {
+        id: 1,
+        title: 'Transaction1 ',
+        amount: 5000,
+        type: 'deposit',
+        category: 'Food',
+        created_at: new Date(),
+      },
+    ]);
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { Summary } from '../../components/Summary';
 import { TransactionsTable } from '../../components/TransactionsTable';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 export function Dashboard(): JSX.Element {
+  useEffect(() => {
+    api.get('/transactions').then(response => {
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <Container>
       <Summary />
